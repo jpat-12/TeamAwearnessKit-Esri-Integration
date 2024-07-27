791aa9eba2358bee65ccdeee78928c94fd0b3f8c
jpat-12 <79032412+jpat-12@users.noreply.github.com>
2024-07-27 08:13:12 -0500

    Create csv-download.py

---
+++ csv-download.py
@@ -0,0 +1,13 @@
+from arcgis.gis import GIS
+from arcgis.features import FeatureLayer
+
+# Connect to the GIS
+gis = GIS()
+
+# Access the feature layer
+url = " INSERT FEATURE SERVRE LINK HERE"
+layer = FeatureLayer(url)
+
+# Query and download data
+features = layer.query(where="1=1", return_geometry=True, out_fields="*").sdf
+features.to_csv("survey.csv", index=False)
