2e5ceaa1fdd39d9747cf3737ba1244034726c9da
jpat-12 <79032412+jpat-12@users.noreply.github.com>
2024-08-16 19:17:01 -0500

    Update arcgis.sh

--- scripts/ubuntu/arcgis.sh
+++ scripts/ubuntu/arcgis.sh
@@ -354,6 +354,39 @@ else
         exit 1
     fi
 fi 
+
+# Add Logging capabilities for /var/www/html/cot-logged.txt
+echo "Adding logging capabilities for the /var/www/html/cot-logged.txt file" 
+cat <<EOF > /opt/TAK-Esri/copy-cot-intake.py 
+import shutil
+import os
+from datetime import datetime
+
+# Define source and base destination directories
+source_file = '/var/www/html/cot-logged.txt'
+base_destination_dir = '/var/www/html/cot-messages-logged/'
+
+# Get current UTC date and time
+current_utc_time = datetime.utcnow()
+date_folder = current_utc_time.strftime('%d-%B-%Y')
+formatted_time = current_utc_time.strftime('%H:%M:%S')
+
+# Create destination directory path for the current day
+destination_dir = os.path.join(base_destination_dir, date_folder)
+os.makedirs(destination_dir, exist_ok=True)
+
+# Create destination file path
+destination_file = os.path.join(destination_dir, f'cot-messages-pulled-{formatted_time}.txt')
+
+# Copy the file
+shutil.copy2(source_file, destination_file)
+
+print(f'File copied to {destination_file}')
+EOF
+## Add the cron job
+crontab -e
+0 * * * * /usr/bin/python3 /opt/TAK-Esri/copy-cot-intake.py
+
 # Reload the daemon and start the service
 sudo systemctl daemon-reload
 sudo systemctl enable feature-layer-update.service
